2e8d4725e3078f5972565ee738ba21001b36977b
Charlie Schmidt <schmidtc@gmail.com>
2016-05-19 14:07:41 -0400

    adding helper function

--- gsTiles/__init__.py
+++ gsTiles/__init__.py
@@ -2,3 +2,22 @@
 #gsTiles
 
 """
+from tileDB import TileDB
+from tileMaker import Tiler
+from npShp.shpReader import shapeFile
+from locator import BBoxLocator
+
+
+def create_source(fname, max_zoom = 12):
+    """
+    Creates a gsTiles file from the given shapefile.
+    pre-rendering upto max_zoom.
+    """
+    shp = shapeFile(fname)
+    dbname = fname.rsplit('.', 1)[0]+'.gsTiles.sqlite'
+    db = TileDB(dbname)
+    db.setup()
+    polys = BBoxLocator(shp)
+    t = Tiler(polys, db)
+    t.fullRender(max_zoom)
+    db.close()
