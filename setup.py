a4402bfad08c9633b225635e4e7e56f7fcfa5673
Charlie Schmidt <schmidtc@gmail.com>
2016-05-20 13:55:16 -0400

    changes to setup.py to support pypi distro

--- setup.py
+++ setup.py
@@ -5,11 +5,22 @@ setup.py for gsTiles
 from distutils.core import setup 
 
 setup(name = 'gsTiles',
+      version = "0.0.1",
       description = 'gsTiles: Python Library for making gsTiles',
       author = 'Charles R Schmidt',
       author_email = 'charlie@gwikis.com',
       url = 'https://github.com/schmidtc/gsTiles',
-      version = "0.0.1",
+      license='MIT',
+      classifiers = [
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+      ],
+      keywords = 'mapping',
+      install_requires=['cairo', 'rtree'],
+      extras_require={'dev': ['Sphinx', 'coverage', 'nose']},
       packages = ['gsTiles',
                   'gsTiles.npShp'],
      )
